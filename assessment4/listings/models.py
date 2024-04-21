c191f485761b0a8ee00e17a9f5c11738026d7285
Charles Lovelace <cwlovelace@gmail.com>
2024-04-21 12:51:45 -0400

    initial

---
+++ assessment4/listings/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
